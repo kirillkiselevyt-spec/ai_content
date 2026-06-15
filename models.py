912500e3236d63405d16d12f2363931ec79c7c90
KIRILLO1D <kirillkiselevyt@gmail.com>
2026-06-15 15:02:21 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,14 +1,11 @@
 from sqlalchemy import Column, Integer, String, Text
 from database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, unique=True, index=True)
-
-    niche = Column(String)
-    audience = Column(String)
-    style = Column(String)
-
-    history = Column(Text, default="")
+    id            = Column(Integer, primary_key=True, index=True)
+    user_id       = Column(String, unique=True, index=True)
+    message_count = Column(Integer, default=0)
+    history       = Column(Text, default="")
